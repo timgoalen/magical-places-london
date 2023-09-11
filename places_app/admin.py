f56895d8777c902d1ec359825fe4966077a6ec34
Tim Goalen <timgoalen@hotmail.com>
2023-09-11 15:19:29 +0000

    Link comments to places and allow StackedInline summaries in admin

--- places_app/admin.py
+++ places_app/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-
 from .models import Place, Comment
 
-admin.site.register(Place)
+
+class CommentInline(admin.StackedInline):
+    # could change to '(admin.TabularInline)' if needed to display more
+    model = Comment
+    extra = 0
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Comment)
