2b304a7af40c47a32f1fed82285434fc2c9eecf2
Tim Goalen <timgoalen@hotmail.com>
2023-09-14 14:22:56 +0000

    Add create comment functionality

--- places_app/views.py
+++ places_app/views.py
@@ -1,23 +1,21 @@
 # remove TemplateView if not using
-from django.views.generic import TemplateView, ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from django.conf import settings
 from django.shortcuts import render
 from .models import Place, Comment
 from django.conf import settings
+from .forms import CommentForm
 
 
 def home_page_view(request):
-    # places = Place.objects.all()
-    # comments = Place.comment_set
     # Return a list of dictionaries for each row in the database
-    # (specifying the 3 values hides the Primary Key number)
+    # (specifying the 3 values hides the Primary Key number) [not done at the moment]
     context = {
-        # "places": places,
-        # "comments": comments,
-        # "places": list(Place.objects.values("place_name", "latitude", "longitude")),
         "places": list(Place.objects.values()),
-        # "comments": list(Comment.objects.values("place_name", "comment")),
         "api_key": settings.GOOGLE_MAPS_API_KEY,
     }
     return render(request, "home.html", context)
@@ -28,9 +26,44 @@ class ListPageView(ListView):
     template_name = "list_view.html"
 
 
-class PlaceDetailView(DetailView):
+class CommentGet(DetailView):
     model = Place
     template_name = "place_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Place
+    form_class = CommentForm
+    template_name = "place_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.place_name = self.object
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        place = self.get_object()
+        return reverse("place_detail", kwargs={"pk": place.pk})
+
+
+class PlaceDetailView(View):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 
 class PlaceCreateView(CreateView):
