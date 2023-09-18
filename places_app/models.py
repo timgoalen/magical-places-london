0c36ef34b38070600ba364cccc76195a3d944d75
Tim Goalen <timgoalen@hotmail.com>
2023-09-18 10:43:52 +0000

    Update Place model with new fields

--- places_app/models.py
+++ places_app/models.py
@@ -1,17 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 
 class Place(models.Model):
-    place_name = models.CharField(max_length=100)
+    place_name = models.CharField(max_length=100, unique=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    contributer = models.ForeignKey(User, on_delete=models.SET("deleted_user"))
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now=True)
+    favourited = models.ManyToManyField(
+        User, related_name="place_favourited", blank=True
+    )
+
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return self.place_name
 
     def get_absolute_url(self):
         return reverse("place_detail", kwargs={"pk": self.pk})
+
+    def number_of_times_favourited(self):
+        return self.favourited.count()
 
 
 class Comment(models.Model):
