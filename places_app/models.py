055e74955c39e1708c91ff1bb9c8fc189d845a25
Tim Goalen <timgoalen@hotmail.com>
2023-09-20 08:20:34 +0000

    Update Place model with deleted_user default

--- places_app/models.py
+++ places_app/models.py
@@ -2,17 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+# Set 'deleted user' for when user creates a Place then deletes account
+deleted_user, _ = User.objects.get_or_create(username="deleted_user")
+
 
 class Place(models.Model):
     place_name = models.CharField(max_length=100, unique=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
     # check use for 'related_name': (if needed)
-    contributer = models.ForeignKey(User, on_delete=models.SET("deleted_user"), related_name="my_places")
-    updated_on = models.DateTimeField(auto_now=True)
-    created_on = models.DateTimeField(auto_now=True)
+    contributer = (
+        models.ForeignKey(
+            User, on_delete=models.SET(deleted_user), related_name="places"
+        ),
+    )
+    updated_on = models.DateTimeField(auto_now=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     favourited = models.ManyToManyField(
-        User, related_name="place_favourited", blank=True
+        User, related_name="favourited_places", blank=True
     )
 
     class Meta:
@@ -29,7 +36,7 @@ class Place(models.Model):
 
 
 class Comment(models.Model):
-    place_name = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="comments")
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="comments")
     comment = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now=True)
