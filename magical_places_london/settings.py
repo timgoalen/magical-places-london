e894f30ad8a229daf0531cecc944fa702ea9f3e8
Tim Goalen <timgoalen@hotmail.com>
2023-11-08 11:27:25 +0000

    Deployment commit

--- magical_places_london/settings.py
+++ magical_places_london/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-timgoalen-magicalplaces-xikc2aho5al.ws-eu104.gitpod.io",
@@ -141,11 +141,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-# STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
+STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
